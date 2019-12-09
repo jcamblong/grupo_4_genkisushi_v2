80fdebd1ac27129bc444881dc7bb289603de2649
jcamblong <eltrujuy@gmail.com>
2019-12-09 15:08:54 -0300

    Se agrega ruteo de EJS y se mueven html a la carpeta views con extension ejs.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Genki Sushi' });
+});
+
+router.get('/agregar-producto', function(req, res) {
+  res.render('productAdd', { title: 'Genki Sushi: Agregar Producto' });
+});
+
+router.get('/carrito', function(req, res) {
+  res.render('productCart', { title: 'Genki Sushi: Carrito' });
+});
+
+router.get('/productos', function(req, res) {
+  res.render('productDetail', { title: 'Genki Sushi: Productos' });
+});
+
+router.get('/registro', function(req, res) {
+  res.render('register', { title: 'Genki Sushi: Registración' });
 });
 
 module.exports = router;
