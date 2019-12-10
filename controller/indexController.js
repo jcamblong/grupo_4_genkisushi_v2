0ff1de15d9a58c37f8eb8369aeaf14b00af2d924
caroguzman <57491106+caroguzman@users.noreply.github.com>
2019-12-10 00:28:42 -0300

    Update indexController.js

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,1 +1,22 @@
+let indexController ={
+    'index': function(req, res) {
+                  res.render('index');
+},
 
+    'productAdd': function(req, res) {
+                  res.render('productAdd');
+},
+    'productCart': function(req, res) {
+                  res.render('productCart');
+},
+
+    'productDetail': function(req, res) {
+                  res.render('productDetail');
+},
+
+    'register': function(req, res) {
+                  res.render('register');
+}
+};
+
+module.exports= indexController;
