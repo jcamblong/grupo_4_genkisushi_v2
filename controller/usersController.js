d933bf1b428dbd136e9ec9568758c310b3ca19d5
jcamblong <eltrujuy@gmail.com>
2020-01-12 23:37:12 -0300

    Se realizan modificaciones en el form de registros en base a campos del JSON. Registro de usuarios ya es funcional. Validacion de usuario existente. Password con hash

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const path = require('path');
+const bcrypt = require('bcrypt');
 let {check, validationResult, body} = require('express-validator');
 
-const productsFilePath = path.join(__dirname, '../data/users.json');
-const users = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const usersFilePath = path.join(__dirname, '../data/users.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 let usersController ={
     'form': function(req, res) {
@@ -18,6 +19,35 @@ let usersController ={
     },
     'register': function(req, res) {
         res.render('register');
+    },
+    'saveUser': function(req, res) {
+        let result = validationResult(req)
+        let search = users.find(function(user){
+            return user.email == req.body.email;
+            })
+
+        console.log(result, search)
+
+        if(result.isEmpty() && typeof search == 'undefined'){
+            let newUser = {
+                        "nombre": req.body.nombre, 
+                        "apellido":req.body.apellido, 
+                        "email": req.body.email,
+                        "password": bcrypt.hashSync(req.body.password, 10),
+                        "calle": req.body.calle,
+                        "altura": req.body.altura,
+                        "entreCalles": req.body.entreCalles,
+                        "localidad": req.body.localidad,
+                        "telefono": req.body.telefono
+                        }
+            users.push(newUser)
+
+            let usersJSON = JSON.stringify(users)
+            fs.writeFileSync('./data/users.json', usersJSON)
+
+            res.redirect('/users/login')
+          
+        } else {res.render('register', {errors: result.errors, data: req.body})}
     }
 };
 
--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,6 @@ let usersController = require('../controller/usersController.js');
 let {check, validationResult, body} = require('express-validator');
 
 
-
-/* GET users listing. */
 router.get('/login', usersController.form);
 
 router.post('/login', [
@@ -13,16 +11,11 @@ router.post('/login', [
   check('password').isLength({min:6, max:12}).withMessage('La contraseña debe tener entre 6 y 12 caracteres')
 ], usersController.login);
 
-router.get('/registro', usersController.register);
+router.get('/register', usersController.register);
 
-router.post('/registro', [
-  check('email').isEmail().withMessage('El email ingresado no es valido'),
-  check('password').isLength({min:6, max:12}).withMessage('El password debe tener entre 6 y 8 caracteres')
-], function(req, res){
-  let result = validationResult(req)
-  if(!result.isEmpty()){
-    res.redirect(301,'/users/registro', {errors: result.errors, data: req.body})
-  } res.redirect(301, '/users/login')
-});
+router.post('/register', [
+  check('email').isEmail().withMessage('Email invallido'),
+  check('password').isLength({min:6, max:10}).withMessage('El password debe tener entre 6 y 10 caracteres')
+], usersController.saveUser);
 
 module.exports = router;
