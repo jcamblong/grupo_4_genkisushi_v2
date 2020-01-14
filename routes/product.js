2223226900984e015f2b6465c6cd9ee4c373cc56
caroguzman <57491106+caroguzman@users.noreply.github.com>
2020-01-14 01:20:28 -0300

    agrego rutas para edit y delete de productos

--- routes/product.js
+++ routes/product.js
@@ -14,6 +14,14 @@ router.post('/create', productController.store); /* POST - Store in DB */
 
 //
 
+//EDIT Product by id | CGR
+router.get('/edit/:id', productController.edit);
+router.put('/edit/:id', productController.update);
+
+//DELETE Product by id  | CGR
+router.delete('/delete/:id', productController.destroy); 
+
+
 router.get('/carrito', productController.productCart);
 
 
