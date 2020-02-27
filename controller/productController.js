687fe7ddd81897bdac837efb02f801732234a381
ckpd84 <ckpd84@gmail.com>
2020-02-27 00:31:06 -0300

    modifico metodo index p/que lea categorias y tipos

--- controller/productController.js
+++ controller/productController.js
@@ -3,23 +3,30 @@ const path = require('path');
 /*agrego db al controlador*/
 const db = require("../database/models");
 
-
-
 const productsFilePath = path.join(__dirname, '../data/productos.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 let productController ={
-    'index': function(req, res) {
+
+	'index': function(req, res) {
 		db.products.findAll()
-			.then(function(products){
-				res.render('products', {products: products})
-			})
+			.then(products =>{
+				let categorias =[];
+				let tipos =[];
+				db.categories.findAll()
+			.then(categorias => {
+				db.product_types.findAll()
+			.then(tipos => {
+				res.render('products', {products: products, categorias: categorias, tipos: tipos});
+			});
+			});
+			});
     },
     'productDetail': function(req, res) {
 		db.products.findByPk(req.params.id)
-		.then(function(product){
-			res.render('productDetail', {product: product})
-		})
+			.then(function(product){
+				res.render('productDetail', {product: product})
+			})
     },
     'productAdd': function(req, res) {
 		db.products.findAll()
@@ -44,44 +51,64 @@ let productController ={
 // Update - Form to edit GET | CGR
 /*Paso de json a db el método edit por get*/
 	'edit': (req, res) => {
-		
-		db.products.findOne({
-			where: {id: req.params.id}
+		db.products.findOne({where: {id: req.params.id}})
 			//incluir categoria y tipo de producto
 			/*include:[{association: 'categorie'}]*/
-		})
-		.then(products =>{
+			.then(products =>{
 				//buscar todas las categorias y todo los productos para cargar el form
-				let categorias =[];
-				let tipos =[];
+			let categorias =[];
+			let tipos =[];
 				db.categories.findAll()
-				.then(categorias => {
-						db.product_types.findAll()
-						.then(tipos => {
-							res.render('productEdit', {products: products, categorias: categorias, tipos: tipos});
+			.then(categorias => {
+				db.product_types.findAll()
+			.then(tipos => {
+				res.render('productEdit', {products: products, categorias: categorias, tipos: tipos});
 			});
 			});
 			});
 	},
 
-
 	// Update - Method to update PUT| CGR
 	'update': (req, res, next) => {
+		db.categories.findOne({where: {name: req.body.inputCategory}})
+			.then(categoria =>{
+				db.product_types.findOne({where: {name: req.body.inputType}})
+			.then(tipo => {
+				if(req.files.length != 0){
+				db.products.update({
+					name: req.body.name,
+					detail:req.body.detail,
+					image: req.files[0].filename,
+					//categoria, tipo
+					category_id: categoria.id,
+					type_id: tipo.id
+				},{where: {id: req.params.id}})
+			.then(res.redirect('/products/detail/'+ req.params.id));
+				} else {
+				db.products.update({
+					name: req.body.name,
+					detail:req.body.detail,
+					//image: req.files[0].filename,
+					//categoria, tipo
+					category_id: categoria.id,
+					type_id: tipo.id	
+				},{where: {id: req.params.id}})
+			.then(res.redirect('/products/detail/'+ req.params.id));
+					}	
+			})
+			})		
+	},
 
-	/*	let arrayIndex;
-
+		/*	let arrayIndex;
 		let product = products.find(function (p, index) {
 			if (p.id == req.params.id){
 				arrayIndex = index;
 				return true;
 			}
 			return false;
-		});
-			
+		});	
 		let editado;
-		
 		console.log(req.files)
-
 		if(req.files.length != 0){ 
 			editado = {
 			...product,
@@ -94,74 +121,24 @@ let productController ={
 				...req.body
 				}
 			}
-
 		products[arrayIndex] = editado;
-
 		fs.writeFileSync(productsFilePath, JSON.stringify(products));
-
-		*/
-
-		db.categories.findOne({
-			where: {name: req.body.inputCategory}
-		})
-		.then(categoria =>{
-			db.product_types.findOne({
-				where: {name: req.body.inputType}
-			})
-		.then(tipo => {
-			if(req.files.length != 0){
-				db.products.update({
-				name: req.body.name,
-				detail:req.body.detail,
-				image: req.files[0].filename,
-				//categoria, tipo
-				category_id: categoria.id,
-				type_id: tipo.id
-	
-				},{
-					where: {id: req.params.id}
-				}).then(res.redirect('/products/detail/'+ req.params.id));
-
-		} else{
-				db.products.update({
-				name: req.body.name,
-				detail:req.body.detail,
-				//image: req.files[0].filename,
-				//categoria, tipo
-				category_id: categoria.id,
-				type_id: tipo.id	
-				},{
-					where: {id: req.params.id}
-				}).then(res.redirect('/products/detail/'+ req.params.id));
-		}
-
-			
-		}
-		
-		)
-	})
-
-			
-	},
+		*/	
 
 	// Delete - Delete one product from DB DELETE | CGR
 	// Delete on DB
 	'destroy': (req, res) => {
-		db.products.destroy({
-			where: {id: req.params.id}
-		})
-		.then(res.redirect('/products'))
+		db.products.destroy({where: {id: req.params.id}})
+			.then(res.redirect('/products'))
 	}
 		
 			/*let products = fs.readFileSync(productsFilePath , {encoding: 'utf-8'});
 		products = JSON.parse(products);
-        
         var filtered = products.filter(function(value, index, arr){
                 return value.id != req.params.id;
                 });
-
         fs.writeFileSync(productsFilePath, JSON.stringify(filtered));
-*/
+			*/
 		
 };
 
