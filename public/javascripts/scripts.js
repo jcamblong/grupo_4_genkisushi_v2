b12bc3b191631f197b23daa4743ed6c9dd85dfde
ckpd84 <ckpd84@gmail.com>
2020-02-27 19:22:20 -0300

    alert con cierre de sesión

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,13 @@
+window.addEventListener ('load', function(){
+    document.querySelector(".btn-logout")
+        .onclick = function(){
+            alert ('Tu sesión se cerró correctamente')
+        }
+
+
+
+
+
+
+
+})
