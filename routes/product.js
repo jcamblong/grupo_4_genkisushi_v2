ed6964206d5c31223e948cf02eb43ef6473c3f51
jcamblong <eltrujuy@gmail.com>
2020-03-09 14:15:23 -0300

    Se modifica armado de nombres en multer usuarios y productos. Se corrige metodo update de productos

--- routes/product.js
+++ routes/product.js
@@ -1,39 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-let productController = require('../controller/productController.js');
-const path = require ('path');
-const multer = require ('multer');
+let productController = require("../controller/productController.js");
+const path = require("path");
+const multer = require("multer");
 
 var storage = multer.diskStorage({
-	destination: function(req, file, cb){
-		cb(null, 'public/img')
-	},
-	filename: function(req,file,cb){
-		cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-	}
-})
-var upload = multer ({storage:storage})
+  destination: function(req, file, cb) {
+    cb(null, "public/img/products");
+  },
+  filename: function(req, file, cb) {
+    cb(null, req.body.name + path.extname(file.originalname));
+  }
+});
+var upload = multer({ storage: storage });
 
 //All products
-router.get('/', productController.index); 
+router.get("/", productController.index);
 //Product detail
-router.get('/detail/:id', productController.productDetail);
+router.get("/detail/:id", productController.productDetail);
 
-/*** CREATE ONE PRODUCT ***/ 
-router.get('/create', productController.productAdd); /* GET - Form to create */
-router.post('/create', upload.any(), productController.store); /* POST - Store in DB */
+/*** CREATE ONE PRODUCT ***/
+
+router.get("/create", productController.productAdd); /* GET - Form to create */
+router.post(
+  "/create",
+  upload.any(),
+  productController.store
+); /* POST - Store in DB */
 
 //EDIT Product by id | CGR
-router.get('/edit/:id', productController.edit);
-router.put('/edit/:id', upload.any(), productController.update);
+router.get("/edit/:id", productController.edit);
+router.put("/edit/:id", upload.any(), productController.update);
 
 //DELETE Product by id  | CGR
-router.delete('/delete/:id', productController.destroy); 
+router.delete("/delete/:id", productController.destroy);
 
 //CART
-router.get('/cart', productController.productCart);
-
+router.get("/cart", productController.productCart);
 
 module.exports = router;
-
-
--- routes/users.js
+++ routes/users.js
@@ -1,112 +1,135 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-let usersController = require('../controller/usersController.js');
-let {check, validationResult, body} = require('express-validator');
-const path = require ('path');
-const multer = require ('multer');
-const userRoute = require('../middlewares/userRoute');
-const guestRoute = require('../middlewares/guestRoute');
-
+let usersController = require("../controller/usersController.js");
+let { check, validationResult, body } = require("express-validator");
+const path = require("path");
+const multer = require("multer");
+const userRoute = require("../middlewares/userRoute");
+const guestRoute = require("../middlewares/guestRoute");
 
 var storage = multer.diskStorage({
-	destination: function(req, file, cb){
-		cb(null, 'public/img/users')
-	},
-	filename: function(req,file,cb){
-		cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-	}
-})
-var upload = multer ({storage:storage})
+  destination: function(req, file, cb) {
+    cb(null, "public/img/users");
+  },
+  filename: function(req, file, cb) {
+    cb(null, req.body.email + path.extname(file.originalname));
+  }
+});
+var upload = multer({ storage: storage });
 
 //LOGIN
-router.get('/login', guestRoute, usersController.loginForm);
+router.get("/login", guestRoute, usersController.loginForm);
 
-router.post('/login', [
-  check('email').isEmail().withMessage('El email ingresado no es valido')
-], usersController.login);
+router.post(
+  "/login",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("El email ingresado no es valido")
+  ],
+  usersController.login
+);
 
-router.get('/logout', usersController.logout);
-
+router.get("/logout", usersController.logout);
 
 //REGISTRO
-router.get('/register', guestRoute, usersController.create);
+router.get("/register", guestRoute, usersController.create);
 
-router.post('/register', upload.any(), [
-  check('name')
-                .isLength({min:1})
-                .withMessage('Debes ingresar tu nombre'),
-  check('lastName')
-                .isLength({min:1})
-                .withMessage('Debes ingresar tu apellido'),
-  check('email')
-                .isEmail()
-                .withMessage('Email invallido'),
-  check('password')
-                .isLength({min:6, max:10})
-                .withMessage('El password debe tener entre 6 y 10 caracteres')
-                .custom((value,{req, loc, path}) => {
-                  if (value !== req.body.repeatPassword) {
-                      // trow error if passwords do not match
-                      throw new Error("Las contraseñas no coinciden");
-                  } else { return value }
-                  }),
-  check('repeatPassword')
-                .isLength({min:1})
-                .withMessage('Debe confirmar la contraseña'),
-  check('street')
-                .isLength({min:1})
-                .withMessage('Debes ingresar un calle'),
-  check('stNumber')
-                .isLength({min:1})
-                .withMessage('Debes ingresar un altura'),
-  check('phone')
-                .isNumeric()
-                .isLength({min:10, max:10})
-                .withMessage('Formato: Codigo de area sin cero + Numeros sin espacios ni guiones')
-], usersController.store);
+router.post(
+  "/register",
+  upload.any(),
+  [
+    check("name")
+      .isLength({ min: 1 })
+      .withMessage("Debes ingresar tu nombre"),
+    check("lastName")
+      .isLength({ min: 1 })
+      .withMessage("Debes ingresar tu apellido"),
+    check("email")
+      .isEmail()
+      .withMessage("Email invallido"),
+    check("password")
+      .isLength({ min: 6, max: 10 })
+      .withMessage("El password debe tener entre 6 y 10 caracteres")
+      .custom((value, { req, loc, path }) => {
+        if (value !== req.body.repeatPassword) {
+          // trow error if passwords do not match
+          throw new Error("Las contraseñas no coinciden");
+        } else {
+          return value;
+        }
+      }),
+    check("repeatPassword")
+      .isLength({ min: 1 })
+      .withMessage("Debe confirmar la contraseña"),
+    check("street")
+      .isLength({ min: 1 })
+      .withMessage("Debes ingresar un calle"),
+    check("stNumber")
+      .isLength({ min: 1 })
+      .withMessage("Debes ingresar un altura"),
+    check("phone")
+      .isNumeric()
+      .isLength({ min: 10, max: 10 })
+      .withMessage(
+        "Formato: Codigo de area sin cero + Numeros sin espacios ni guiones"
+      )
+  ],
+  usersController.store
+);
 
 //USER PROFILE
-router.get('/user', userRoute, usersController.show);
-
+router.get("/user", userRoute, usersController.show);
 
 //USER EDIT
-router.get('/editUser', userRoute, usersController.edit)
-router.put('/editUser',upload.any(), [
-  check('name')
-                .isLength({min:1})
-                .withMessage('Debes ingresar tu nombre'),
-  check('lastName')
-                .isLength({min:1})
-                .withMessage('Debes ingresar tu apellido'),
-  check('street')
-                .isLength({min:1})
-                .withMessage('Debes ingresar un calle'),
-  check('stNumber')
-                .isLength({min:1})
-                .withMessage('Debes ingresar un altura'),
-  check('phone')
-                .isNumeric()
-                .isLength({min:10, max:10})
-                .withMessage('Formato: Codigo de area sin cero + Numeros sin espacios ni guiones')
-], usersController.update);  
+router.get("/editUser", userRoute, usersController.edit);
+router.put(
+  "/editUser",
+  upload.any(),
+  [
+    check("name")
+      .isLength({ min: 1 })
+      .withMessage("Debes ingresar tu nombre"),
+    check("lastName")
+      .isLength({ min: 1 })
+      .withMessage("Debes ingresar tu apellido"),
+    check("street")
+      .isLength({ min: 1 })
+      .withMessage("Debes ingresar un calle"),
+    check("stNumber")
+      .isLength({ min: 1 })
+      .withMessage("Debes ingresar un altura"),
+    check("phone")
+      .isNumeric()
+      .isLength({ min: 10, max: 10 })
+      .withMessage(
+        "Formato: Codigo de area sin cero + Numeros sin espacios ni guiones"
+      )
+  ],
+  usersController.update
+);
 
 //EDIT PASSWORD
-router.get('/changePassword', userRoute, usersController.changePasswordForm);
-router.put('/changePassword',[
-  check('password')
-                .isLength({min:6, max:10})
-                .withMessage('El password debe tener entre 6 y 10 caracteres')
-                .custom((value,{req, loc, path}) => {
-                if (value !== req.body.repeatPassword) {
-                // trow error if passwords do not match
-                throw new Error("Las contraseñas no coinciden");
-                } else { return value }
-                }),
-  check('repeatPassword')
-                .isLength({min:1})
-                .withMessage('Debe confirmar la contraseña'),
-] 
-, usersController.changePassword);
-
+router.get("/changePassword", userRoute, usersController.changePasswordForm);
+router.put(
+  "/changePassword",
+  [
+    check("password")
+      .isLength({ min: 6, max: 10 })
+      .withMessage("El password debe tener entre 6 y 10 caracteres")
+      .custom((value, { req, loc, path }) => {
+        if (value !== req.body.repeatPassword) {
+          // trow error if passwords do not match
+          throw new Error("Las contraseñas no coinciden");
+        } else {
+          return value;
+        }
+      }),
+    check("repeatPassword")
+      .isLength({ min: 1 })
+      .withMessage("Debe confirmar la contraseña")
+  ],
+  usersController.changePassword
+);
 
 module.exports = router;
