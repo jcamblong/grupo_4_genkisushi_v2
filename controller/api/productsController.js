834433c3db5f962a2b967698d06b0af67d3a0605
ckpd84 <ckpd84@gmail.com>
2020-03-27 13:20:45 -0300

    modifico api/products

--- controller/api/productsController.js
+++ controller/api/productsController.js
@@ -3,14 +3,18 @@ const db = require("../../database/models");
 let productsController = {
 
     list: function (req, res) {
-        db.products.findAll({attributes: ['id', 'name', 'detail', 'type_id', 'category_id']})
+        db.products.findAll({attributes: ['id', 'name', 'detail', 'type_id', 'category_id', 'categories.name']}, {association: "categories"}, {association: "product_types"})
             .then(products => {
                 for(let i = 0; i < products.length; i++) {
                 products[i].setDataValue("endpoint", "/api/products/" + products[i].id)
                 }
                 let respuesta = {
-                count: products.length,
-                products
+                    meta: {
+                        status: 200,
+                        count: products.length,
+                        url: "/api/products"
+                    },
+                    data: products
                 }
             res.json(respuesta)        
             })
@@ -22,11 +26,17 @@ let productsController = {
             'createdAt',
             'updatedAt'
             ]}})
-            .then(product => {
-            product.setDataValue("image", "http://localhost:3000/img/products/" + product.image)
-            res.json(product)
-            })
-        }
+            .then(product => { product.setDataValue("image", "http://localhost:3000/img/products/" + product.image)
+            let producto = {
+                meta: {
+                    status: 200,
+                    url: "/api/products/" + product.id
+                },
+                data: product
+            }
+        res.json(producto)        
+        })
     }
+}
 
 module.exports = productsController;
